fa927fc45c6665e6380295c13c28a2b296087a8a
JY <90738099+JY9150@users.noreply.github.com>
2025-08-16 00:30:38 +0800

    update fake-p path

--- install/fake-p.py
+++ install/fake-p.py
@@ -1,18 +1,23 @@
+"""Module to run the PIME server using a 64-bit Python interpreter."""
+
 import subprocess
-from pathlib import Path
 
-# this this script that will modify the PIME Server to run on 64-bit Python
+# This script modifies the PIME Server to run on 64-bit Python.
 
-# Create venv
+# Create venv.
 
-# Pip install the required packages
-# Multilingual_ime & pyinstaller
+# Pip install the required packages.
+# Multilingual_ime & pyinstaller.
+# 1. `pyinstaller -F this_script.py`
+# 2. Move the generated executable to the
+#    PIME\python\python3 and rename it to python.exe.
 
 # Path to the 64-bit Python interpreter (in the virtual environment)
-venv_python_path = Path(__file__).parent / "venv" / "Scripts" / "python.exe"
+VENV_PYTHON_PATH = (
+    r"C:\Program Files (x86)\PIME\python\input_methods\MIME-win\venv\Scripts\python.exe"
+)
 
 # The script to run (can be passed as an argument)
-script_to_run = "server.py"
-
+SCRIPT_TO_RUN = r"C:\Program Files (x86)\PIME\python\server.py"
 # Execute the 64-bit Python interpreter with the specified script
-subprocess.run([venv_python_path, script_to_run])
+subprocess.run([VENV_PYTHON_PATH, SCRIPT_TO_RUN], check=True)
